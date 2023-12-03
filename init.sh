1cc36a39b3de0e7776f24e5e6d615eff9ba170ef
Takuya Mannami <takuya.mannami@gmail.com>
2023-12-03 19:44:38 +0900

    update

--- init.sh
+++ init.sh
@@ -6,7 +6,6 @@ go install github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest
 go install github.com/rogpeppe/godef@latest
 go install github.com/jstemmer/gotags@latest
 go install golang.org/x/tools/cmd/goimports@latest
-go install github.com/x-motemen/gore/cmd/gore@latest
 
 mkdir -p ~/.emacs.d/git && \
     cd ~/.emacs.d/git && \
