eca1dd6288762a3283dbfc978f87d137ad7d6d1d
Takuya Mannami <takuya.mannami@gmail.com>
2023-12-07 01:52:16 +0900

    update

--- go-install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz && rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz && rm go1.21.5.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
 ./tools-install.sh 
-./go-install.sh
 ./go-tools-install.sh 
 ./set-env.sh
