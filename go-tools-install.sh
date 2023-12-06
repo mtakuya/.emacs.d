42279f79435fa547ef1cff2bf5abcffc4b69d23c
Takuya Mannami <takuya.mannami@gmail.com>
2023-12-07 01:35:28 +0900

    update

--- go-tools-install.sh
+++ go-tools-install.sh
@@ -9,7 +9,7 @@ go install github.com/rogpeppe/godef@latest
 go install github.com/jstemmer/gotags@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
-
 go install github.com/matsuu/kataribe@latest
 go install github.com/matsuu/go-mysql-query-digest@latest
 go install github.com/google/pprof@latest
+go install github.com/pressly/goose/v3/cmd/goose@latest
