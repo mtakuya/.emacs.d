496f42025ec80061ef3759285350cd1682571649
Takuya Mannami <takuya.mannami@gmail.com>
2023-12-07 01:13:30 +0900

    update

--- tools-install.sh
+++ tools-install.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/usr/bin/bash
 apt-get update
-apt-get install -y git make wget emacs dstat git-lfs graphviz gv sysstat jq mysqltuner
+apt-get install -y git make wget emacs dstat git-lfs graphviz gv sysstat jq mysqltuner unzip
+
+wget https://github.com/tkuchiki/alp/releases/download/v1.0.21/alp_linux_amd64.zip
+unzip alp_linux_amd64.zip
+install ./alp /usr/local/bin
+rm alp_linux_amd64.zip
