c4d7a6e6c579bfde1f7a181d5f41eeb1dd6b5b5d
Takuya Mannami <takuya.mannami@gmail.com>
2023-12-06 19:24:54 +0900

    update

--- go-install.sh
+++ go-install.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-apt-get update
-apt-get install -y git make wget emacs
 wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz && rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
-source ~/.profile
-cd .emacs.d/
-./init.sh
--- go-modules-install.sh
+++ go-modules-install.sh
@@ -7,3 +7,4 @@ go install github.com/rogpeppe/godef@latest
 go install github.com/jstemmer/gotags@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/matsuu/kataribe@latest
