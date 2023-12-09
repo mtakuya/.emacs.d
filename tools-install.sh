b1a6507ad67c5634a96017daf07780f5392e8553
Takuya Mannami <takuya.mannami@gmail.com>
2023-12-10 01:37:30 +0900

    update

--- tools-install.sh
+++ tools-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 apt-get update
-apt-get install -y git make wget emacs dstat git-lfs graphviz gv sysstat jq mysqltuner unzip htop tcpdump net-tools strace
+apt-get install -y git make wget emacs dstat git-lfs graphviz gv sysstat jq mysqltuner percona-toolkit unzip htop tcpdump net-tools strace
 
 wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz && rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz && rm go1.21.5.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
