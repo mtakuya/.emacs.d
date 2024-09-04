a9ff5a1f14078e0a9cdc49da2f537bd41ba21d6a
Takuya Mannami <takuya.mannami@gmail.com>
2024-09-05 01:17:36 +0900

    update

--- init.el
+++ init.el
@@ -13,7 +13,6 @@
         ("org" . "https://orgmode.org/elpa/")))
 
 (setq make-backup-files nil)
-(setq auto-save-default nil)
 (setq create-lockfiles nil)
 (setq ring-bell-function 'ignore)
 
@@ -28,6 +27,10 @@
 
 (global-hl-line-mode t)
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(hl-line ((t (:background "gray10")))))
 
 (define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
@@ -81,7 +84,8 @@
 
 (use-package company
   :ensure t)
-(global-company-mode) 
+(global-company-mode)
+(add-hook 'after-init-hook 'global-company-mode)
 (setq company-idle-delay 0) 
 (setq company-minimum-prefix-length 2)
 (setq company-tooltip-limit 10)
@@ -251,6 +255,13 @@
 (global-set-key (kbd "C-c y") 'company-yasnippet)
 (global-set-key (kbd "C-c C-y") 'company-yasnippet)
 
+(show-paren-mode t)
+
+(define-key company-active-map (kbd "\C-n") 'company-select-next)
+(define-key company-active-map (kbd "\C-p") 'company-select-previous)
+(define-key company-active-map (kbd "\C-d") 'company-show-doc-buffer)
+(define-key company-active-map (kbd "M-.") 'company-show-location)
+
 ;; -------------------------------------------------------------
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -258,10 +269,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages '(flycheck gopls use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
 ;; -------------------------------------------------------------
