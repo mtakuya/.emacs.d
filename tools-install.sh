d8c7f98bda1e0c0a8c1d21883fe6de6b0b662498
Takuya Mannami <takuya.mannami@gmail.com>
2024-09-05 01:50:40 +0900

    update

--- tools-install.sh
+++ tools-install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 apt-get update
-apt-get install -y git make wget emacs dstat git-lfs graphviz gv sysstat jq mysqltuner percona-toolkit unzip htop tcpdump net-tools strace
+apt-get install -y git make wget emacs dstat git-lfs graphviz gv sysstat jq mysqltuner percona-toolkit unzip htop tcpdump net-tools strace curl
 
 wget https://go.dev/dl/go1.23.0.linux-amd64.tar.gz && rm -rf /usr/local/go && tar -C /usr/local -xzf go1.23.0.linux-amd64.tar.gz && rm go1.23.0.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 wget https://github.com/tkuchiki/alp/releases/download/v1.0.21/alp_linux_amd64.zip
 unzip alp_linux_amd64.zip
