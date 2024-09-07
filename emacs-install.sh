27516380f583502e3c863579e7afcdac50f7e38d
Takuya Mannami <takuya.mannami@gmail.com>
2024-09-07 20:01:07 +0900

    update

--- emacs-install.sh
+++ emacs-install.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install -y build-essential libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo autoconf
+sudo apt install -y build-essential libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo autoconf libgccjit-12-dev gnutls-bin
 
 git clone --depth 1 --branch emacs-29 https://git.savannah.gnu.org/git/emacs.git
 
@@ -8,7 +8,7 @@ git checkout emacs-29
 
 ./autogen.sh
 
-./configure --with-pgtk --with-native-compilation
+CFLAGS='-I/usr/lib/gcc/x86_64-linux-gnu/12/include -L/usr/lib/gcc/x86_64-linux-gnu/12' ./configure --prefix=/usr/local --with-native-compilation --with-gnutls=ifavailable --without-pop --with-pgtk
 
 make -j$(nproc)
 sudo make install
