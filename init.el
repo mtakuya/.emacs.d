ea85b01d51b4844ce365545030cdade3422734c9
Takuya Mannami <takuya.mannami@gmail.com>
2024-09-15 05:27:38 +0900

    update

--- init.el
+++ init.el
@@ -38,7 +38,9 @@
 (save-place-mode 1)
 (electric-pair-mode 1)
 (which-function-mode 1)
+(setq which-func-unknown "N/A")
 (show-paren-mode t)
+
 ;; Move between windows using the Shift key and arrow keys.
 (windmove-default-keybindings)
 
@@ -438,12 +440,35 @@
   (setq prescient-aggressive-file-save t)
   (prescient-persist-mode +1))
 
+(use-package request
+  :ensure t)
+
+;; -------------------------------------------------------------
+
+(defun extract-webpage-content (url)
+  (interactive "sEnter URL: ")
+  (request url
+    :parser 'buffer-string
+    :success
+    (cl-function (lambda (&key data &allow-other-keys)
+                   (when data
+                     (with-current-buffer (get-buffer-create "*Webpage Content*")
+			   (erase-buffer)
+			   (insert data)
+			   (html-mode)
+			   (display-buffer (current-buffer))))))
+    :error
+    (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
+                   (message "Got error: %S" error-thrown)))
+    :complete
+    (lambda (&rest _) (message "Finished!"))))
+
 ;; -------------------------------------------------------------
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(dirvish flycheck gopls use-package)))
+ '(package-selected-packages '(request dirvish flycheck gopls use-package)))
 
 ;; -------------------------------------------------------------
