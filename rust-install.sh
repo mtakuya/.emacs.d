22392c443f50c41e41fe9b591f409d2cbfdc52ff
Takuya Mannami <takuya.mannami@gmail.com>
2024-09-20 01:35:05 +0900

    update

--- rust-install.sh
+++ rust-install.sh
@@ -2,5 +2,4 @@
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-rustup component add rust-analyzer
-rustup component add rust-src
+rustup component add clippy rust-analysis rust-src rust-docs rustfmt rust-analyzer
