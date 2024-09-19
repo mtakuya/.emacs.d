dccfa2a96900344687c3cc628151151a4c4fb954
Takuya Mannami <takuya.mannami@gmail.com>
2024-09-20 01:48:26 +0900

    update

--- init.el
+++ init.el
@@ -46,7 +46,6 @@
 
 (define-key global-map (kbd "C-x C-j") 'execute-extended-command)
 (define-key global-map (kbd "C-x j") 'execute-extended-command)
-(define-key global-map (kbd "C-c e") 'eldoc-doc-buffer)
 
 (set-face-background 'mode-line "gray10")
 (set-face-foreground 'mode-line "gray95")
@@ -308,8 +307,10 @@
 
 (use-package eglot
   :ensure t
-  :hook ((rust-mode ruby-mode go-mode) . eglot-ensure))
+  :hook ((rust-mode ruby-mode go-mode c-mode c++-mode) . eglot-ensure))
 (with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs '((c-mode) "clangd"))
+  (add-to-list 'eglot-server-programs '((c++-mode) "clangd"))
   (add-to-list 'eglot-server-programs '((go-mode) "gopls"))
   (add-to-list 'eglot-server-programs '((ruby-mode ruby-ts-mode) "ruby-lsp"))
   (add-to-list 'eglot-server-programs
--- tools-install.sh
+++ tools-install.sh
@@ -4,4 +4,4 @@ touch ~/.profile
 source ~/.profile
 
 sudo apt-get update
-sudo apt-get install -y git make wget emacs dstat git-lfs graphviz gv sysstat jq mysqltuner percona-toolkit unzip htop tcpdump net-tools strace curl nasm ripgrep
+sudo apt-get install -y git make wget emacs dstat git-lfs graphviz gv sysstat jq mysqltuner percona-toolkit unzip htop tcpdump net-tools strace curl nasm ripgrep clangd
